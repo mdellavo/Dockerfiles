1f5be1e51caafac07cd3783897d74a32dc7cf92e
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2014-09-09 23:20:15 -0400

    touch

--- base/start.sh
+++ base/start.sh
@@ -5,12 +5,10 @@ set -x
 USER=marc
 PASSWORD=`pwgen -1`
 
-useradd -m "${USER}" -s /bin/bash -p `mkpasswd ${PASSWORD}`
-usermod -G sudo "${USER}"
-echo "User: ${USER} / Password: ${PASSWORD}"
+useradd -p `mkpasswd ${PASSWORD}` "${USER}"
+#echo "User: ${USER} / Password: ${PASSWORD}"
 
 echo "Run parts"
-ls /start.d/
 run-parts -v /start.d/
 
 echo "Starting ssh..."
