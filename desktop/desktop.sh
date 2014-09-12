a1fe14f50d9ac53271f9f8b3e72eb15a5695fd78
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2014-09-12 08:29:30 -0400

    fluxbox

--- desktop/desktop.sh
+++ desktop/desktop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo -u marc -H bash -c "xpra start :7"
+DISPLAY=10
+SCREEN_SIZE="800x600"
+DISPLAY_1=$(( $DISPLAY + 1 ))
+
+sudo -u marc -H bash -c "xpra start :$DISPLAY --start-child=\"Xephyr -ac -screen $SCREEN_SIZE -query localhost -host-cursor -reset -terminate :$DISPLAY_1\" --xvfb=\"Xvfb +extension Composite -screen 0 ${SCREEN_SIZE}x24+32 -nolisten tcp -noreset -auth /home/marc/.Xauthority\" --no-notifications --no-pulseaudio & sleep 5 ; DISPLAY=:$DISPLAY_1 fluxbox &"
